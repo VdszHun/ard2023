f766563c250fbb31fbfd24970b107b43b265cdaa
vadaszdavid <vadaszdavid@EOTVOS2.LOCAL>
2023-11-20 13:00:51 +0100

    7th commit

--- app/Http/Controllers/HomersekletController.php
+++ app/Http/Controllers/HomersekletController.php
@@ -3,10 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Homerseklet;
 
 class HomersekletController extends Controller
 {
     public function index(){
-        return view('homerseklet');
+        $homersekletek = Homerseklet::orderBy('h_id', 'DESC')->paginate(10);
+        return view('homerseklet',['homersekletek' => $homersekletek]);
+    }
+
+    //API
+    public function create(Request $req){
+        $validalas = $Validator::make(
+            $req->all(),
+            [
+                "homerseklet" => "required"
+                "paratartalom" => "required"
+            ],
+            [
+                "homerseklet.required" => "Hiányzó hőmérséklet"
+                "paratartalom.required" => "Hiányzó páratartalom"
+            ]
+        );
+        if($validalas->fails()){
+            $data['message'] = "Hibás adatok";
+            $data['errorList'] = $validalas->messages();
+            return response()->json($data,400);
+        }else{
+            //Adatbázisba szúrni az adatokat
+            $homerseklet = new Homerseklet;
+            $homerseklet->hofok = $req->input('homerseklet')
+            $homerseklet->paratartalom = $req->input('paratartalom');
+            $homerseklet->meres_ideje = date('Y-m-d H:i:s');
+            $homerseklet->save();
+            return response()->json($homerseklet,201);
+        }
     }
 }
--- resources/views/homerseklet.blade.php
+++ resources/views/homerseklet.blade.php
@@ -1,6 +1,32 @@
 @extends('layouts.master')
 @section('title', "Főoldal")
 @section('content')
-Hőmérséklet
+
+<div>
+    <h1>Hőmérsékletek</h1>
+
+    <div class="table-responsive">
+        <table class="table table-primary table-stripped table-hover">
+            <tr>
+                <th>Id</th>
+                <th>Hőmérséklet</th>
+                <th>Páratartalom</th>
+                <th>Mérés ideje</th>
+            </tr>
+            @foreach ($homersekletek as $hom)
+            <tr>
+                <td>{{$hom->h_id}}</td>
+                <td>{{$hom->hofok}}&deg;C</td>
+                <td>{{$hom->paratartalom}}%</td>
+                <td>{{$hom->meres_ideje}}</td>
+            </tr>
+            @endforeach
+
+        </table>
+
+        {{$homersekletek->links() }}
+    </div>
+</div>
+
 @endsection
 
