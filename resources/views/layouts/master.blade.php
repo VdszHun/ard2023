8f22967349dd2160161b8868fb650ef8e77c1e86
vadaszdavid <vadaszdavid@EOTVOS2.LOCAL>
2023-11-20 11:38:50 +0100

    3rd commit

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -13,7 +13,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 </head>
 <body>
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
             <div class="col-4 bg-dark text-white">
                 <ul>
